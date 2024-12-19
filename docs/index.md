608af361212cd8e82df9f0d82402f9e98294e896
Carl A. Adams <carl.adams@planet.com>
2024-12-19 10:27:29 -0800

    Squashed commit of draft initial CI/CD branch.

--- docs/index.md
+++ docs/index.md
@@ -6,6 +6,6 @@ Planet auth utility code.
 
 The code in this library is general auth utility code, and does not make
 any assumptions about the environment in which it is operating.  The
-[Planet SDK for Pythin](https://developers.planet.com/docs/pythonclient/)
+[Planet SDK for Python](https://developers.planet.com/docs/pythonclient/)
 leverages this library, and is pre-configured for the Planet Cloud Service used
 by customers.
--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import nox
 
 nox.options.stop_on_first_error = True
@@ -23,7 +26,7 @@ _ALL_PYTHON = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
 @nox.session(python=_ALL_PYTHON)
 def pytest(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
 
     options = session.posargs
     if "-k" in options:
@@ -34,83 +37,125 @@ def pytest(session):
 
 @nox.session(python=_DEFAULT_PYTHON)
 def semgrep_src(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     # session.run("semgrep", "scan", "--strict", "--verbose", "--error", "--junit-xml", "--junit-xml-output=semgrep-src.xml", "src")
     session.run("semgrep", "scan", "--strict", "--verbose", "--error", "src")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def black_lint(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("black", "--verbose", "--check", "--diff", "--color", ".")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def black_format(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("black", "--verbose", ".")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def mypy(session):
-    session.install("-e", ".[tests, examples]")
+    session.install("-e", ".[test, examples]")
     session.run("mypy", "--install-type", "--non-interactive", "--junit-xml", "mypy.xml")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pyflakes_src(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("pyflakes", "src")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pyflakes_examples(session):
-    session.install("-e", ".[tests, examples]")
+    session.install("-e", ".[test, examples]")
     session.run("pyflakes", "docs/examples")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pyflakes_tests(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("pyflakes", "tests")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pylint_src(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("pylint", "src")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pylint_examples(session):
-    session.install("-e", ".[tests, examples]")
+    session.install("-e", ".[test, examples]")
     session.run("pylint", "docs/examples")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
 def pylint_tests(session):
-    session.install("-e", ".[tests]")
+    session.install("-e", ".[test]")
     session.run("pylint", "--disable", "protected-access", "--disable", "unused-variable", "tests")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
-def build_wheel(session):
+def pkg_build_wheel(session):
     session.install("-e", ".[build]")
     session.run("pyproject-build")
     # session.run("simple503", "-B", "dist", "dist")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
-def build_local_dist(session):
+def pkg_build_local_dist(session):
     session.install("-e", ".[build]")
     session.run("pyproject-build")
     session.run("simple503", "-B", "dist", "dist")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
+def pkg_check(session):
+    session.install("-e", ".[build]")
+    session.run("twine", "check", "--strict", "dist/*.whl", "dist/*.tar.gz")
+
+
+def _publish_pypi(session, repo_url, token):
+    session.install("-e", ".[build]")
+    session.run(
+        "twine",
+        "upload",
+        "--non-interactive",
+        # "--verbose",
+        "--username",
+        "__token__",
+        "--password",
+        token,
+        "--repository-url",
+        repo_url,
+        # "--repository",
+        # repo_name,
+        "dist/*.whl",
+        "dist/*.tar.gz",
+    )
+
+
+@nox.session(python=_DEFAULT_PYTHON)
+def pkg_publish_pypi_prod(session):
+    token = os.getenv("NOX_PYPI_API_TOKEN")
+    if not token:
+        sys.exit("NOX_PYPI_API_TOKEN must be set in the environment with the PyPi access token")
+    _publish_pypi(session, "https://upload.pypi.org/legacy", token)
+
+
+@nox.session(python=_DEFAULT_PYTHON)
+def pkg_publish_pypi_test(session):
+    token = os.getenv("NOX_PYPI_API_TOKEN")
+    if not token:
+        sys.exit("NOX_PYPI_API_TOKEN must be set in the environment with the PyPi access token")
+    _publish_pypi(session, "https://test.pypi.org/legacy/", token)
+
+
+@nox.session(python=_DEFAULT_PYTHON)
 def mkdocs_build(session):
     session.install("-e", ".[docs]")
-    session.run("mkdocs", "-v", "build")
+    session.run("mkdocs", "-v", "build", "--clean")
 
 
 @nox.session(python=_DEFAULT_PYTHON)
@@ -120,14 +165,11 @@ def mkdocs_serve(session):
 
 
 @nox.session(python=_DEFAULT_PYTHON)
-def pyblish_pypi(session):
-    session.install("-e", ".[build]")
-    # TODO
-    assert False
-
-
-@nox.session(python=_DEFAULT_PYTHON)
-def pyblish_readthedocs(session):
+def mkdocs_publish_readthedocs(session):
     session.install("-e", ".[build, docs]")
-    # TODO
+    # TODO - Manual doc publushing
+    print(
+        "ERROR: Read The Docs publishing not implemented in the noxfile."
+        "  Documentation publishing is triggered via GitHub webhook."
+    )
     assert False
