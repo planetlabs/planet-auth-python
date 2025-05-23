3a13aea3fdc7169ae2fe1ac7ca7df3fcbbe2531b
Carl A. Adams <carl.adams@planet.com>
2025-05-23 12:15:49 -0700

    disallow setting invalid profiles

--- src/planet_auth_utils/commands/cli/profile_cmd.py
+++ src/planet_auth_utils/commands/cli/profile_cmd.py
@@ -269,6 +269,10 @@ def cmd_profile_set(selected_profile):
     """
     if not selected_profile:
         selected_profile = _dialogue_choose_auth_profile()
+    else:
+        # Validate user input.  Dialogue selected profiles should be pre-vetted.
+        normalized_profile_name, _ = PlanetAuthFactory.load_auth_client_config_from_profile(selected_profile)
+        selected_profile = normalized_profile_name
 
     try:
         user_profile_config_file = PlanetAuthUserConfig()
