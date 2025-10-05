1040170ae812740d6bc5ce8771b5505df9c82262
Carl Alexander Adams <carlalex@overlords.com>
2025-10-05 08:14:59 -0700

    cherry-picking some small incidental changes from a larger feature branch to front-run the main MR.

--- src/planet_auth/oidc/token_validator.py
+++ src/planet_auth/oidc/token_validator.py
@@ -14,7 +14,7 @@
 
 import jwt
 import time
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
 
 import planet_auth.logging.auth_logger
 from planet_auth.auth_exception import AuthException, InvalidTokenException
@@ -257,9 +257,18 @@ class TokenValidator:
         return validated_claims
 
     @staticmethod
-    def hazmat_unverified_decode(token_str):
-        # WARNING: Treat unverified token claims like toxic waste.
-        #          Nothing can be trusted until the token is verified.
+    @InvalidArgumentException.recast(jwt.exceptions.DecodeError)
+    def hazmat_unverified_decode(token_str) -> Tuple[Dict, Dict, Any]:
+        """
+        Decide a JWT without verifying the signature or any claims.
+
+        !!! Warning
+            Treat unverified token claims with extreme caution.
+            Nothing can be trusted until the token is verified.
+
+        Returns:
+            Returns the decoded JWT header, payload, and signature
+        """
         unverified_complete = jwt.decode_complete(token_str, options={"verify_signature": False})  # nosemgrep
         return unverified_complete["header"], unverified_complete["payload"], unverified_complete["signature"]
 
